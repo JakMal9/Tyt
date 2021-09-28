fe4ade46c5652a801d5a248b60a3cb53c4d7e3be
JakMal9 <86560744+JakMal9@users.noreply.github.com>
2021-09-28 13:48:17 +0200

    Tyt 16 (#6)

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Short instruction how to run weltyt.py in Docker:
 - In Terminal enter into this directory where files are.
-- Build an image to Docker by command: 'docker build -t weltyt .'
-- Run image by command: 'docker run -it weltyt'
+- Download there Makefile.
+- Build an image to Docker by command: 'dob'
+- Run image by command: 'dor'
+- Or do it in one move by command: 'dobr'
 - If you have problem with permission try run it as root.
 - If you would like avoid permanent running as root, follow this instruction please: (https://github.com/sindresorhus/guides/blob/main/docker-without-sudo.md)
 # How to open weltyt.py without Docker:
--- weltyt.py
+++ weltyt.py
@@ -1,10 +1,5 @@
 import sqlite3
 
-def initializationDB(cursor):
-
-        with open("TYTDB.sql") as base:
-                for line in base:
-                        cursor.execute(line)
 
 def initializationDB(cursor):
 
@@ -12,18 +7,18 @@ def initializationDB(cursor):
         for line in base:
             cursor.execute(line)
 
+
 def show_my_resources(x):
     for row in cur.execute("SELECT * FROM resources"):
-            print(row)
+        print(row)
 
     for rows in cur.execute("SELECT * FROM lands"):
         print(rows)
 
+
 def buy_plants(plants):
-    amount = input(
-                    "How many plants do you want to buy? Write only integer please. "
-                )
-            
+    amount = input("How many plants do you want to buy? Write only integer please. ")
+
     try:
         amount = int(amount)
 
@@ -46,46 +41,47 @@ def buy_plants(plants):
         )
         print("Well done plants deal. ")
     except ValueError:
-        
+
         print("Invalid input, write integer only please. ")
 
+
 def buy_lands(lands):
-    show_me_lands = input("If you want see lands offert, write yes please. \
+    show_me_lands = input(
+        "If you want see lands offert, write yes please. \
         \
-        ")
+        "
+    )
     if show_me_lands == "yes":
         print(lands_offert)
 
     else:
         print("Go directly to buying ")
 
-        
         chosen_class = input(
             "What land class do you want to buy? Write only properly names of lands please. \
             \
             "
         )
-    
-        try: 
-            
+
+        try:
+
             add_land = cur.execute(
-                f'INSERT INTO lands VALUES ("{lands_offert[choose_class]["class"]}", {lands_offert[choose_class]["id"]}, {lands_offert[choose_class]["growth_rate"]}, {lands_offert[choose_class]["price_ISL"]}, {lands_offert[choose_class]["plants"]});'
+                f'INSERT INTO lands VALUES ("{lands_offert[chosen_class]["class"]}", {lands_offert[chosen_class]["id"]}, {lands_offert[chosen_class]["growth_rate"]}, {lands_offert[chosen_class]["price_ISL"]}, {lands_offert[chosen_class]["plants"]});'
             )
             money_amount = cur.execute(
                 'SELECT amount  FROM resources WHERE type = "Money";'
             ).fetchone()[0]
             lands_price = cur.execute(
-                f'SELECT price FROM lands WHERE class = "{choose_class}";'
+                f'SELECT price FROM lands WHERE class = "{chosen_class}";'
             ).fetchone()[0]
             new_money_amount = money_amount - lands_price
             cur.execute(
                 f'UPDATE resources SET amount = {new_money_amount}  WHERE type = "Money";'
             )
-        
+
             print(f"Buying {chosen_class} is done properly ")
         except KeyError:
             print("Invalid input")
-
 
 
 lands_offert = {
@@ -131,59 +127,66 @@ conn = sqlite3.connect("TYTDB.db")
 
 cur = conn.cursor()
 
-
 initializationDB(cur)
 
 conn.commit()
 
-first_choice = input("Choose what would you like to do. If you want check your resources write \"show_my_resources\" please. If you want go directly to buying, write \"buy\". If you want avoid this step write \"no\" please. \
+first_choice = input(
+    'Choose what would you like to do. If you want check your resources write "show_my_resources" please. If you want go directly to buying, write "buy". If you want avoid this step write "no" please. \
     \
-    ")
+    '
+)
 for choice in first_choice:
-
 
     if first_choice == "show_my_resources":
         show_my_resources(first_choice)
-        first_choice = input("Choose what would you like to do. If you want check your resources write \"show_my_resources\" please. If you want go directly to buying, write \"buy\". If you want avoid this step write \"no\" please. \
+        first_choice = input(
+            'Choose what would you like to do. If you want check your resources write "show_my_resources" please. If you want go directly to buying, write "buy". If you want avoid this step write "no" please. \
             \
-            ")
+            '
+        )
 
     elif first_choice == "buy":
-        what_buy = input("To start grow tobbaco you need plants and lands. To buy plants write just \"Plants\", to buy lands write \"Lands\" please. \
+        what_buy = input(
+            'To start grow tobbaco you need plants and lands. To buy plants write just "Plants", to buy lands write "Lands" please. \
             \
-            ")
+            '
+        )
         if what_buy == "Plants":
             buy_plants(what_buy)
-            first_choice = input("Choose what would you like to do. If you want check your resources write \"show_my_resources\" please. If you want go directly to buying, write \"buy\". If you want avoid this step write \"no\" please. \
+            first_choice = input(
+                'Choose what would you like to do. If you want check your resources write "show_my_resources" please. If you want go directly to buying, write "buy". If you want avoid this step write "no" please. \
                 \
-                ")
+                '
+            )
 
         elif what_buy == "Lands":
             buy_lands(what_buy)
-            first_choice = input("Choose what would you like to do. If you want check your resources write \"show_my_resources\" please. If you want go directly to buying, write \"buy\". If you want avoid this step write \"no\" please. \
+            first_choice = input(
+                'Choose what would you like to do. If you want check your resources write "show_my_resources" please. If you want go directly to buying, write "buy". If you want avoid this step write "no" please. \
                 \
-                ")
-                   
-        elif what_buy == "no":
-            print("Shopping is finished ")
-            break
-
+                '
+            )
 
         else:
             print("Invalid input")
-            first_choice = input("Choose what would you like to do. If you want check your resources write \"show_my_resources\" please. If you want go directly to buying, write \"buy\". If you want avoid this step write \"no\" please. \
+            first_choice = input(
+                'Choose what would you like to do. If you want check your resources write "show_my_resources" please. If you want go directly to buying, write "buy". If you want avoid this step write "no" please. \
                 \
-                ")
+                '
+            )
+    elif first_choice == "no":
+            print("Shopping is finished ")
+            break
 
+    else:
+        print("Invalid input")
+        first_choice = input(
+            'Choose what would you like to do. If you want check your resources write "show_my_resources" please. If you want go directly to buying, write "buy". If you want avoid this step write "no" please. \
+            \
+            '
+        )
 
-
-else:
-    print("Invalid input")
-    first_choice = input("Choose what would you like to do. If you want check your resources write \"show_my_resources\" please. If you want go directly to buying, write \"buy\". If you want avoid this step write \"no\" please. \
-        \
-        ")
-
-print("Shopping is finished")
 print("Go forward")
 
 conn.close()
